e8928e7c0970f8b21ad670ca5ebd6d96816e3649
Eva Nunez <evahlivkova@MacBook-Pro-2.local>
2025-03-29 16:42:04 +0100

    upload solution

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const goodbye = (text) => {
+  const rozlouceni = document.querySelector(".email__closing");
+  rozlouceni.textContent = (text);
+}
 
+// const rozlouceni = goodbye("Na shledanou, Eva");
+
+const fillSubject = (subject) => {
+  const predmet = document.querySelector(".email__subject");
+  predmet.textContent = (subject)
+}
+
+const predmet = fillSubject("Nejlepsi nabidka prace ever")
+
+const fillBody = (body, text) => {
+  const telo = document.querySelector(".email__body");
+  telo.textContent = body;
+  goodbye(text)
+}
+
+const telo = fillBody("tak me tu mate!", "Na shledanouuuuu, Eva")
